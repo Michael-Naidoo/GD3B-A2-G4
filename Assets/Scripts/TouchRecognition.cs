52672154ebc2fcc30b88c5f14bbf6fd1660f17a1
Michael-Naidoo <130663291+Michael-Naidoo@users.noreply.github.com>
2024-08-12 10:45:15 +0200

    PokeBall Movement 1
    Ball follows finger on screen, (you can also use the mouse to test this in the editor without connecting a phone)

--- Assets/Scripts/TouchRecognition.cs
+++ Assets/Scripts/TouchRecognition.cs
@@ -10,10 +10,17 @@ public class TouchRecognition : MonoBehaviour
         if (Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
-            Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
-            touchPosition.z = 0;
+            Vector3 touchPosition = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, Camera.main.WorldToScreenPoint(transform.position).z));
+            touchPosition.z = transform.position.z; // Maintain the object's Z position
             transform.position = touchPosition;
             Debug.Log(touchPosition);
+        }
+        else
+        {
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.WorldToScreenPoint(transform.position).z));
+            mousePosition.z = transform.position.z; // Maintain the object's Z position
+            transform.position = mousePosition;
+            Debug.Log(mousePosition);
         }
     }
 }

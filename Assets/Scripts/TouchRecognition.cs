97bd067fb5228bf0df86462f7e53ab4aa4039c5a
Michael-Naidoo <130663291+Michael-Naidoo@users.noreply.github.com>
2024-08-13 12:48:12 +0200

    reset velocity
    set velocity to 0 when the player picks up the ball, this fixed the problem of the ball gaining downward velocity when held.

--- Assets/Scripts/TouchRecognition.cs
+++ Assets/Scripts/TouchRecognition.cs
@@ -1,9 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TouchRecognition : MonoBehaviour
 {
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
     // Update is called once per frame
     void Update()
     {
@@ -13,14 +21,14 @@ public class TouchRecognition : MonoBehaviour
             Vector3 touchPosition = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, Camera.main.WorldToScreenPoint(transform.position).z));
             touchPosition.z = transform.position.z; // Maintain the object's Z position
             transform.position = touchPosition;
-            Debug.Log(touchPosition);
+            rb.velocity = Vector3.zero;
         }
         else
         {
             Vector3 mousePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.WorldToScreenPoint(transform.position).z));
             mousePosition.z = transform.position.z; // Maintain the object's Z position
-            transform.position = mousePosition;
-            Debug.Log(mousePosition);
+            //transform.position = mousePosition;
         }
+        Debug.Log(rb.velocity);
     }
 }

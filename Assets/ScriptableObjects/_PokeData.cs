182b02da5a4e9314597af8d125426fc5b9d37c60
JayQkew <rayjebb@gmail.com>
2024-08-19 13:50:58 +0200

    Scriptable object giving me issues

--- Assets/ScriptableObjects/_PokeData.cs
+++ Assets/ScriptableObjects/_PokeData.cs
@@ -25,11 +25,3 @@ public class _PokeData : ScriptableObject
     public float baseFleeRate;
 }
 
-[CreateAssetMenu]
-[Serializable]
-// This is a ScriptableObject of the Players Pokemon
-public class _Pokemon : ScriptableObject
-{
-    public List<PokeStats> player_pokemon = new List<PokeStats>();
-    //here
-}

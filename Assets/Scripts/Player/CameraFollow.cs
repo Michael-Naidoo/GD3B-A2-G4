90cae72d10043780765e04bc6bb02d6050e64167
JayQkew <rayjebb@gmail.com>
2024-08-20 21:39:17 +0200

    Camera Follow

--- Assets/Scripts/Player/CameraFollow.cs
+++ Assets/Scripts/Player/CameraFollow.cs
@@ -4,5 +4,20 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
+    private Transform player;
+    private Vector3 offset;
 
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        offset = transform.position - player.position;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 targetPos = player.position + offset;
+
+        transform.position = Vector3.Lerp(transform.position, targetPos, Time.deltaTime * 2);
+
+    }
 }
